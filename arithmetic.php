a341fbdcc52092874025c76b58e3a9164ebf5f94
Pablo Pinero <pinerop@gmail.com>
2015-03-12 13:45:06 -0500

    more php

--- arithmetic.php
+++ arithmetic.php
@@ -4,32 +4,60 @@ $a = 300;
 $b = 121;
 
 function add($a, $b) {
-    echo $a + $b . PHP_EOL;
+	if (is_numeric($a) && is_numeric($b)){
+    	return $a + $b . PHP_EOL;
+	} else {
+		errorMessage($a,$b);
+	}	
 }
 
 function subtract($a, $b) {
-    echo $a - $b . PHP_EOL;
+	if (is_numeric($a) && is_numeric($b)) {
+    	return $a - $b . PHP_EOL;
+    } else {
+    	errorMessage($a,$b);
+    }
 }
 
 function multiply($a, $b) {
-    echo $a * $b . PHP_EOL;
+	if (is_numeric($a) && is_numeric($b)) {
+    	return $a * $b . PHP_EOL;
+    } else {
+    	errorMessage($a,$b);
+    }
 }
 
 function divide($a, $b) {
-    echo $a / $b . PHP_EOL;
+	if (is_numeric($a) && is_numeric($b)) {
+		if($a !== 0){
+    		return $a / $b;
+		}
+		else{
+			return false;
+		}
+	}
 }
 
 function modulus($a, $b) {
-	echo $a % $b . PHP_EOL;
+	if (is_numeric($a) && is_numeric($b)) {
+		return $a % $b;
+	} else {
+		errorMessage($a,$b);
+	}
+}
+
+function errorMessage($a){
+	echo "input error {$a}, {$b}" . PHP_EOL;
 }
 
 add(1, 7);
-subtract(9,2);
+subtract(9,"constrictor");
 multiply(98,321);
-divide(98,54);
+divide(0,54);
 modulus(900, 65);
 
 echo "=================
+/\/\/\/\/\/\/\/\/
 =================" . PHP_EOL;
 
 add($a, $b);
