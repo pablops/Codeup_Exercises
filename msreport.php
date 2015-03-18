12fb5e0bb6ce6ba8e8470f6033444fa7dfe9b42c
Pablo Pinero <pinerop@gmail.com>
2015-03-18 13:31:57 -0500

    added some sorting code but it is not yet finished

--- msreport.php
+++ msreport.php
@@ -21,7 +21,20 @@ echo $explodedAtNewLine[2][0], "  |  ", $explodedAtNewLine[1][0], "  |  ", $expl
 echo "==================================================================================================\n";
 echo $explodedAtNewLine[4][0], "  |   ", $explodedAtNewLine[4][1], "  |  ", $explodedAtNewLine[4][2], "  |  ", $explodedAtNewLine[4][3], $explodedAtNewLine[4][4], PHP_EOL;
 
-for($i = 6; $i < 13; $i++){
-	$x = 0;
-	echo $explodedAtNewLine[$i][$x], "                   ", $explodedAtNewLine[$i][$x + 1], "             ", $explodedAtNewLine[$i][$x + 2], "             ", $explodedAtNewLine[$i][$x + 3],  PHP_EOL;
+$maxValueSales = 12;
+
+for($i = 6; $i < 39; $i++){
+	$x = 0;	
+	echo "max v sales is  $maxValueSales\n";
+	if ($explodedAtNewLine[$i][$x + 3] == $maxValueSales){
+		$maxValueSales = $maxValueSales - 1;
+		echo "the thing i'm measuring is ", $explodedAtNewLine[$i][$x + 3] . PHP_EOL;
+		echo $explodedAtNewLine[$i][$x + 3], "                   ", $explodedAtNewLine[$i][$x + 1], "             ", $explodedAtNewLine[$i][$x + 2], "             ", $explodedAtNewLine[$i][$x + 0],  PHP_EOL;
+		// $maxValueSales = $explodedAtNewLine[$i][$x + 3];
+		$maxValueSales = $maxValueSales - 1;
+		echo "i is $i";
+		echo "max value sales at end of loop is  $maxValueSales\n"; 
+	} else {
+		$maxValueSales = $maxValueSales - 1;
+	}
 }
