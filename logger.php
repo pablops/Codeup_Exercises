a6b66346f3e2438d1063cf6d0193cd7888cdefb5
Pablo Pinero <pinerop@gmail.com>
2015-03-18 15:05:01 -0500

    logs error messages onto file

---
+++ logger.php
@@ -0,0 +1,12 @@
+<?php
+
+function logMessage($logLevel, $message) {
+    $date = date('Y-d-m');
+    $time = date('g:i:s');
+	$filename = "log-$date.txt";
+	$handle = fopen($filename, 'a+');
+	fwrite($handle, $date . " " . $logLevel . " " . $message . PHP_EOL);
+}
+
+logMessage("INFO", "This is an info message.");
+logMessage("ERROR", "This is an info message.");
